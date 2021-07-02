f5064595f0541a0d9d23dedf69187edf645d19d0
Evgeniy Zadorognyh <evg.zadorognyh41@gmail.com>
2021-07-02 16:44:04 +0300

    create repo

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
